2a5a6eb57f9bf70898b33b1bad89107ca49338bb
Kovriga-dono <84527696+Kovriga-dono@users.noreply.github.com>
2024-04-15 20:57:19 +0500

    Update import.py

--- test_5/import.py
+++ test_5/import.py
@@ -1,9 +1,12 @@
 from main import create_directories 
 from main import remove_directories
 from random_list import get_random_elem
-create_directories()
-remove_directories()
-get_random_elem()
+
+def call():
+    get_random_elem()
+    create_directories()
+    remove_directories()
+
+    call()
 
 
-# функции вызываются по два раза, пока не понял, в чем ошибка
