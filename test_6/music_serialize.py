13b60317b0267295862ca3d7d31eeec1637df002
Kovriga-dono <84527696+Kovriga-dono@users.noreply.github.com>
2024-04-16 17:26:25 +0500

    Update music_serialize.py

--- test_6/music_serialize.py
+++ test_6/music_serialize.py
@@ -18,3 +18,13 @@ print(pickle_data)
 
 with open('group.pickle', 'wb') as pickle_file:
     pickle.dump(my_favourite_group, pickle_file)
+
+# Задача:
+# Создать модуль music_serialize.py. В этом модуле определить словарь для вашей любимой музыкальной группы, например:
+# my_favourite_group = {
+# ‘name’: ‘Г.М.О.’,
+# ‘tracks’: [‘Последний месяц осени’, ‘Шапито’],
+# ‘Albums’: [{‘name’: ‘Делать панк-рок’,‘year’: 2016},
+# {‘name’: ‘Шапито’,‘year’: 2014}]}
+# С помощью модулей json и pickle сериализовать данный словарь в json и в байты, вывести результаты в терминал. 
+# Записать результаты в файлы group.json, group.pickle соответственно. В файле group.json указать кодировку utf-8.
